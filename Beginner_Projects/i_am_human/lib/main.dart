13982c72b725883b3be8b4a094c762e41ae1f440
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2024-06-21 17:48:10 +0100

    Created I_am_Human App using the knowledged of initially learnt widgets

--- Beginner_Projects/i_am_human/lib/main.dart
+++ Beginner_Projects/i_am_human/lib/main.dart
@@ -9,10 +9,39 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.lightGreenAccent.shade100,
+            title: const Center(
+              child: Text(
+                'I am Human',
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ),
+          body: const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage('./assets/images/human.png'),
+                  width: 250,
+                  height: 250,
+                ),
+                Text('Hi! My name is Bob'),
+                Text('and I am a human'),
+              ],
+            ),
+          ),
         ),
       ),
     );
