d10e22c2a8bac203e8222814b1fdfc94efb8a9b6
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2024-06-21 15:30:50 +0100

    Using only materialApp prebuilt widget for simple Hello World App

--- Beginner_Projects/hello_world/lib/main.dart
+++ Beginner_Projects/hello_world/lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          brightness: Brightness.light,
+        ),
+        title: "Hello World",
+        home: const Center(
+          child: Text(
+            "Hello World!",
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
     );
-  }
-}
