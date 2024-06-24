df3d252e7d5f4aabc047125481e62d040a5c3105
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2024-06-24 23:58:36 +0100

    ListView adapted instead row as well

--- Beginner_Projects/overcoming_overflow/lib/main.dart
+++ Beginner_Projects/overcoming_overflow/lib/main.dart
@@ -14,6 +14,7 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
             child: ListView(
+          scrollDirection: Axis.horizontal,
           children: [
             Container(
               width: 300,
