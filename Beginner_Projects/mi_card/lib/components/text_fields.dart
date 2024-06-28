ad5415addb22f735a9883acfad5130ac0b6180ff
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2024-06-28 12:46:44 +0100

    restyled the resuable widget with card and colors decorations

--- Beginner_Projects/mi_card/lib/components/text_fields.dart
+++ Beginner_Projects/mi_card/lib/components/text_fields.dart
@@ -13,16 +13,27 @@ class IconTextFieldRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.lightGreen[50],
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
       child: Row(
         children: [
-          Icon(icon),
-          const SizedBox(width: 10),
+          const SizedBox(
+            width: 10,
+          ),
+          Icon(
+            icon,
+            color: Colors.lightGreen,
+          ),
+          const SizedBox(
+            width: 15,
+          ),
           Expanded(
             child: TextField(
               decoration: InputDecoration(
+                border: InputBorder.none,
                 hintText: hintText,
               ),
             ),
