7cde47aba0e5a44c0689dd847e2ae49001090994
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2024-06-29 23:47:51 +0100

    retweaking basic app structure

--- Beginner_Projects/programming_tips/lib/main.dart
+++ Beginner_Projects/programming_tips/lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
-        body: Center(
-          child: Text('Programming Tips!'),
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
         ),
       ),
     );
