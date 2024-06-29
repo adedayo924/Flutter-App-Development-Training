6b783d9d294a98bce3e9f1e1e6f83e66dcbfbb44
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2024-06-30 00:01:46 +0100

    added a multi-line comment example using variable types in programming

--- Beginner_Projects/programming_tips/lib/main.dart
+++ Beginner_Projects/programming_tips/lib/main.dart
@@ -9,9 +9,15 @@ void main() {
 // Basic Programming Tips
 
 /*
-This is used for multi-line comments. Example:
+This is used for multi-line comments. 
+Example:
 */
 
+/*
+Different types of Variables:
+- String Variables: Used to store text.
+Example: String name = 'John Doe';
+*/
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
